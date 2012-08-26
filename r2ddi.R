0af562f38e64ad4e0ce10e51f254e51ad925a609
mhebing <mhebing@diw.de>
2012-08-26 21:17:28 +0200

    First part of Stata support

--- r2ddi.R
+++ r2ddi.R
@@ -24,21 +24,24 @@ listToXML <- function(node, sublist){
 #
 # r2ddi is the main function
 #
-r2ddi = function(input_file="data/test.csv", output_file="output/test.xml", mc=FALSE) {
+r2ddi = function(input_file="data/test.csv", output_file=NA, mc=FALSE, csv.sep=",") {
   #
   # Get the file format from the file extension using regexpr
   # *.csv => CSV
   # *.sav => SPSS
   # *.dta => Stata
   #
+  if (is.na(output_file)) output_file = paste(input_file, ".xml", sep="")
   if (regexpr(input_file, pattern="csv$", ignore.case=TRUE) != -1){
-    cat("[INFO] Input format: CSV\n")
-    csv_list = as.list(read.csv(input_file, stringsAsFactors=FALSE))
+    cat("[INFO] Input format: CSV\n[INFO] Read input file... ")
+    csv_list = as.list(read.csv(input_file, stringsAsFactors=FALSE, sep=csv.sep))
+    cat("[DONE]\n[INFO] Run lapply... ")
     varlist = lapply(csv_list, handle_csv)
+    cat("[DONE]\n")
     for(i in names(varlist)) addAttributes(varlist[[`i`]], "name"=i)
   } else if (regexpr(input_file, pattern="sav$", ignore.case=TRUE) != -1){
     cat("[INFO] Input format: SPSS (*.sav)\n")
-    cat("[INFO] Load file... ")
+    cat("[INFO] Read input file... ")
     spss_list = read.spss(input_file, to.data.frame=FALSE, use.value.labels=FALSE)
     cat("[DONE]\n")
     if(mc){
@@ -51,9 +54,23 @@ r2ddi = function(input_file="data/test.csv", output_file="output/test.xml", mc=F
       varlist = lapply(spss_list, handle_spss)
       cat("[DONE]\n")
     }
-    for(i in names(varlist)) addAttributes(varlist[[`i`]], "name"=i)
+    #for(i in names(varlist)) addAttributes(varlist[[`i`]], "name"=i)
   } else if (regexpr(input_file, pattern="dta$", ignore.case=TRUE) != -1){
     cat("[INFO] Input format: Stata (*.dta)\n")
+    cat("[INFO] Read input file... ")
+    input = read.dta(input_file, convert.factors=FALSE, convert.dates=FALSE, missing.type=TRUE)
+    cat("[DONE]\n")
+    stata_list = rep(list(NA), length(names(input)))
+    names(stata_list) = names(input)
+    j = 1
+    for(i in names(input)) {
+      stata_list[[`i`]] = list(name = names(input)[j],
+        data = input[`i`],
+        labl = attr(input, "var.labels")[j],
+        catgry = attr(input, "label.table")[attr(input, "val.labels")[j]] )
+      j = j + 1
+    }
+    varlist = lapply(stata_list, handle_stata)
   } else {
     cat("[ERROR] Don't know the file extension\n")
   }
@@ -70,18 +87,18 @@ handle_csv = function(input){
   if (class(input) == "numeric" || class(input) == "integer") {
     var = newXMLNode("var")
     addAttributes(var, "nature" = "interval")
-    addChildren(var, newXMLNode("sumStat", attrs=c("type" = "min"), min(input, na.rm=TRUE) ))
-    addChildren(var, newXMLNode("sumStat", attrs=c("type" = "max"), max(input, na.rm=TRUE) ))
-    addChildren(var, newXMLNode("sumStat", attrs=c("type" = "valid"), sum(table(input)) ))
-    addChildren(var, newXMLNode("sumStat", attrs=c("type" = "invalid"), length(input) - sum(table(input)) ))
-    addChildren(var, newXMLNode("sumStat", attrs=c("type" = "mean"), mean(input, na.rm=TRUE) ))
-    addChildren(var, newXMLNode("sumStat", attrs=c("type" = "sd"), sd(input, na.rm=TRUE) ))
+    addChildren(var, newXMLNode("sumStat", attrs=c("type" = "min"), min(input, na.rm=TRUE)))
+    addChildren(var, newXMLNode("sumStat", attrs=c("type" = "max"), max(input, na.rm=TRUE)))
+    addChildren(var, newXMLNode("sumStat", attrs=c("type" = "valid"), sum(table(input))))
+    addChildren(var, newXMLNode("sumStat", attrs=c("type" = "invalid"), length(input) - sum(table(input))))
+    addChildren(var, newXMLNode("sumStat", attrs=c("type" = "mean"), mean(input, na.rm=TRUE)))
+    addChildren(var, newXMLNode("sumStat", attrs=c("type" = "sd"), sd(input, na.rm=TRUE)))
     return(var)
   } else if (class(input) == "character") {
     var = newXMLNode("var")
     addAttributes(var, "nature" = "nominal/ordinal")
-    addChildren(var, newXMLNode("sumStat", attrs=c("type" = "valid"), sum(table(input)) ))
-    addChildren(var, newXMLNode("sumStat", attrs=c("type" = "invalid"), length(input) - sum(table(input)) ))
+    addChildren(var, newXMLNode("sumStat", attrs=c("type" = "valid"), sum(table(input))))
+    addChildren(var, newXMLNode("sumStat", attrs=c("type" = "invalid"), length(input) - sum(table(input))))
     t = table(input)
     for(i in 1:length(t)) {
       addChildren(var, newXMLNode("catgry",
@@ -125,9 +142,31 @@ handle_spss = function(input) {
     var = newXMLNode("var")
     return(var)
   }
-  return(var)
 }
 
-
-
+#
+# Function for lapply on Stata data
+#
+handle_stata = function(input) {
+  data = input$data[[input$name]]
+  if(input$catgry == "NULL" && typeof(data) == "integer") {
+    var = newXMLNode("var")
+    addAttributes(var, "nature"="intervall")
+    addChildren(var, newXMLNode("sumStat", attrs=c("type" = "min"), min(data, na.rm=TRUE) ))
+    addChildren(var, newXMLNode("sumStat", attrs=c("type" = "max"), max(data, na.rm=TRUE) ))
+    addChildren(var, newXMLNode("sumStat", attrs=c("type" = "valid"), sum(table(data)) ))
+    addChildren(var, newXMLNode("sumStat", attrs=c("type" = "invalid"), length(data) - sum(table(data)) ))
+    addChildren(var, newXMLNode("sumStat", attrs=c("type" = "mean"), mean(data, na.rm=TRUE) ))
+    addChildren(var, newXMLNode("sumStat", attrs=c("type" = "sd"), sd(data, na.rm=TRUE) ))
+    return(var)
+  } else if(input$catgry != "NULL") {
+    var = newXMLNode("var")
+    addAttributes(var, "nature" = "nominal/ordinal")
+    t = table(data)
+    return(var)
+  } else {
+    var = newXMLNode("var")
+    return(var)
+  }
+}
 
