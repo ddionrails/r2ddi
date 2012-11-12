f0b926621f0cb0d8d41060f5ee37968a73d3f73f
mhebing <mhebing@diw.de>
2012-11-12 19:30:52 +0100

    ddi2xml: insert comments

--- r2ddi/R/ddi2xml.R
+++ r2ddi/R/ddi2xml.R
@@ -1,12 +1,33 @@
+#
+# ddi2xml(ddi, filename, version)
+#
+# Write ddi-object to an xml-file.
+#
+# Params:
+# * ddi: ddi-object (input)
+# * filename: Output-file
+# * version: DDI-version (default = 2.5)
+#
 ddi2xml = function(ddi, filename, version="2.5") {
 
 
   ##### INTERNAL FUNCTIONS #####
 
+  #
+  # xml2(ddi)
+  #
+  # Produce xml content for DDI 2.5
+  #
   xml2 = function(ddi) {
+
+    # Root element
     content = newXMLNode("codebook")
+
+    # Add dataDscr-node, because it's not possible within the loop
     addChildren(content, newXMLNode("dataDscr"))
-    for( filename in names(ddi$fileDscr) ) {
+
+    # Loop for all (data-)files
+    for( filename in names(ddi[['fileDscr']]) ) {
       addChildren(
         content,
         newXMLNode(
@@ -17,11 +38,16 @@ ddi2xml = function(ddi, filename, version="2.5") {
               newXMLNode(
                 "fileName",
                  filename ))))
-      for( var in ddi$fileDscr[[filename]][["varDscr"]] ) {
+
+      # Loop for variables
+      for( var in ddi[['fileDscr']][[filename]][['varDscr']] ) {
         xmlVar =
           newXMLNode(
             "var",
             attrs = c("ID" = var$name, "files" = filename) )
+
+        # Within the variable-loop:
+        # -> Loop for summary statistics
         for ( stat in names(var$sumStat) ) {
           addChildren(
             xmlVar,
@@ -30,9 +56,13 @@ ddi2xml = function(ddi, filename, version="2.5") {
               attrs = c("type"=stat),
               var$sumStat[[stat]] ))
         }
+
+        # Add variable node to root
         addChildren(content[["dataDscr"]], xmlVar)
       }
     }
+
+    # Return
     return(content)
   }
 
@@ -43,9 +73,15 @@ ddi2xml = function(ddi, filename, version="2.5") {
   ##### START #####
 
   if ( version == "2.5" ) {
-    content = xml2(ddi)
+
+    # Generate xml-document-object
     doc = newXMLDoc()
-    doc = addChildren(doc, content)
+
+    # Add content to xml-document-object,
+    # calling the function for DDI 2.5
+    doc = addChildren(doc, xml2(ddi))
+
+    # Write xml-document-object to xml-file
     saveXML(doc, file = filename)
   }
 }
