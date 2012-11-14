ba08dbc4fbff67a372a8749cf330d74feb006fbd
mhebing <mhebing@diw.de>
2012-11-14 12:09:56 +0100

    Update source.R

---
+++ r2ddi/R/ddi2latex.R
@@ -0,0 +1,11 @@
+#
+# ddi2latex(ddi, file)
+#
+# Exports ddi object as a latex-codebook.
+#
+# Params:
+# * ddi: ddi-object
+# * file: file-name
+#
+ddi2latex = function(ddi, file){
+}
--- source.R
+++ source.R
@@ -1,12 +1,23 @@
-source("r2ddi/R/csv2ddi.R")
-source("r2ddi/R/ddi2csv.R")
+
+### Examples ###
 source("r2ddi/R/ddi.R")
-source("r2ddi/R/ddiExtractor.R")
 # source("r2ddi/R/ddiVariable.R")
+
+### Helper ###
+source("r2ddi/R/ddiExtractor.R")
+source("r2ddi/R/ddi.attachFileDscr.R")
+
+### Imports ###
+source("r2ddi/R/csv2ddi.R")
 source("r2ddi/R/spss2ddi.R")
 source("r2ddi/R/stata2ddi.R")
-source("r2ddi/R/ddi.attachFileDscr.R")
-source("r2ddi/R/ddi2xml.R")
 
-name_file = "r2ddi/data/test.dta"
-name_dataset = "testdata"
+### Exports ###
+source("r2ddi/R/ddi2xml.R")
+source("r2ddi/R/ddi2csv.R")
+source("r2ddi/R/ddi2latex.R")
+
+### Test variables ###
+input = "r2ddi/data/test.dta"
+test_dataset = "testdata"
+output = "test/test.xml"
