7988ac0c2489010af92427cb73ab8c3a6ba82ad2
mhebing <mhebing@diw.de>
2013-03-07 11:23:07 +0100

    Bring back attachFileDscr

---
+++ r2ddi/R/attachFileDscr.R
@@ -0,0 +1,20 @@
+#' Attach file_dscr from another DDI object
+#'
+#' @param master This is the main object, which will be extended.
+#' @param attachment This object will be attached to the master object.
+#' @export
+ddi.attachFileDscr <-
+  function(master, attachment)
+{
+
+  # TODO: It would be much easier to use a simple c().
+
+  file_list <- names(attachment$file_dscr)
+
+  for( file in file_list )
+  {
+    master$file_dscr[[file]] <- attachment$file_dscr[[file]]
+  }
+
+  return(master)
+}
