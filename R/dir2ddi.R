f3a2569574b2af7fba114658e6733fa9dd311c0e
mhebing <mhebing@diw.de>
2013-08-13 13:52:45 +0200

    Refactor dir2ddi

--- R/dir2ddi.R
+++ R/dir2ddi.R
@@ -5,62 +5,59 @@
 #'                  for the previous three)
 #' @param multicore Use multicore functionallity
 #' @export
-dir2ddi <-
-  function(path, file_type = "all", multicore=TRUE)
-{
+dir2ddi <- function(path, file_type = "all", multicore=TRUE) {
 
-  if(!(file_type %in% c("all", "csv", "sav", "dta")))
-  {
-    cat("Invalid file type:", file_type, "\n")
-    return()
-  } else if(file_type == "all") {
-    pattern <- ".(dta|csv|sav)$"
-  } else {
-    pattern <- paste(".", file_type, "$", sep = "")
+  main <- function() {
+    pattern <- .pattern(file_type)
+    if is.null(pattern) return()
+    file_list <- .file_list(path, pattern)
+
+    if(multicore) {
+      result_list <- mclapply(file_list, .run_stata2ddi(x))
+    } else {
+      result_list <- lapply(file_list, .run_stata2ddi(x))
+    }
+
+    master <- .combine(result_list)
+    return(master)
   }
 
-  file_list <- list.files(path)
-  
-  file_list <-
-    file_list[
-      regexpr(
-        file_list,
-        pattern     = pattern,
-        ignore.case = TRUE) != -1]
-
-  if(multicore)
-  {
-    result_list <-
-      mclapply(
-        file_list,
-        function(x)
-        {
-          stata2ddi(
-            paste(path, x, sep = ""),
-            x,
-            keep_data = FALSE)
-        })
-  } else {
-    result_list <-
-      lapply(
-        file_list,
-        function(x)
-        {
-          stata2ddi(
-            paste(path, x, sep = ""),
-            x,
-            keep_data = FALSE)
-        })
+  .run_stata2ddi <- function(x) {
+    stata2ddi(paste(path, x, sep = ""),
+              x,
+              keep_data = FALSE)
   }
 
-  master <- result_list[[1]]
+  .file_list <- function(path, pattern) {
+    fl <- list.files(path)
+    fl <- fl[regexpr(fl,
+                     pattern     = pattern,
+                     ignore.case = TRUE) != -1]
+    fl
+  }
 
-  if (length(result_list) > 1)
-    for(i in 2:length(file_list))
-      master <-
-        attachFileDscr(
-          master,
-          result_list[[i]])
+  .combine <- function(result_list) {
+    master <- result_list[[1]]
+    if (length(result_list) > 1)
+      for(i in 2:length(file_list))
+        master <-
+          attachFileDscr(
+            master,
+            result_list[[i]])
+  }
 
-  return(master)
+  .pattern <- function(file_type) {
+    if(!(file_type %in% c("all", "csv", "sav", "dta"))) {
+      cat("Invalid file type:", file_type, "\n")
+      pattern <- NULL
+    } else if(file_type == "all") {
+      pattern <- ".(dta|csv|sav)$"
+    } else {
+      pattern <- paste(".", file_type, "$", sep = "")
+    }
+    pattern
+  }
+
+  x <- main()
+  x
 }
